40cbfa61d5e8ce219c9b3b6c02e3a3fada4d1d88
Yogesh Kumbhar <yogeshskumbhar198@gmail.com>
2024-02-15 22:31:57 +0530

    folder structure and files created

--- template.py
+++ template.py
@@ -25,6 +25,7 @@ list_of_files = [
     f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
+    "research/trials.ipynb"
     "templates/index.html",
     "app.py",
     "Dockerfile",
